28e1fc404e49d7c2127fc23a297e48c4e6021f1d
Aluno <aluno@IPIRANGA.FATEC>
2026-05-06 13:51:45 -0300

    Adiciona laco com for e while

---
+++ laço/Perfeito.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Perfeito
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+		System.out.print("Digite um numero positivo: ");
+		int m = sc.nextInt();
+		
+	while(m <= 0){
+	    System.out.println("o numero deve ser positivo");
+	    m = sc.nextInt();
+	}
+	
+	sc.close();
+	int soma = 1;
+	int metade = m / 2;
+	for (int i = 2; i<= metade; i++) {
+	    if (m % i == 0) {
+	        soma = soma + i;
+	    }
+	}
+	    if (soma == m) {
+	        System.out.println(m + "e perfeito");
+	    } else {
+	        System.out.println(m + " nao e perfeito");
+
+	    }
+	}
+}
